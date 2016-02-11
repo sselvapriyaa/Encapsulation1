86d93c027705f90f3a1fd2a7eb7b6be9088ee7d9
Gladwin <Gladwin@Gladwin-THINK>
2016-02-11 14:12:41 -0600

    modified the startup class commit

--- src/lab1/Company.java
+++ src/lab1/Company.java
@@ -10,14 +10,15 @@ package lab1;
  * @author sanandamirtharaj
  */
 public class Company {
-    private HRManager hr;
+    private HRManager hr = new HRManager();
+    
     
     public Company(){
         hr=new HRManager();
     }
     
     public void hireEmployee(String firstName,String lastName,String ssn){
-        
+        hr.hireEmployee(firstName, lastName, ssn);
     }
 
     public void setHr(HRManager hr) {
